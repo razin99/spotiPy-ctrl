438ee7094ff8eb461748de9db37d36624ad34f99
Razin Idzuddin <z5281664@unsw.edu.au>
2020-06-24 20:31:09 +1000

    Implementing vol up, vol down

--- main.py
+++ main.py
@@ -1,4 +1,50 @@
+# https://github.com/moses-palmer/pynput/issues/20 
 from pynput import keyboard
+import spotipy
+import spotipy.util as util
+import creds
+
+scope = 'user-modify-playback-state user-read-playback-state'
+token = util.prompt_for_user_token(creds.USERNAME,
+                           scope,
+                           client_id=creds.CLIENT_ID, 
+                           client_secret=creds.CLIENT_SECRET,
+                           redirect_uri=creds.REDIRECT_URI)
+
+sp = spotipy.client.Spotify(auth=token)
+# device_name = input("Device name: ")
+device_name = "LAPTOP-T34QML4F"
+def get_device(device_name):
+    device_list = sp.devices()['devices']
+    device = next(dev for dev in device_list if dev["name"] == device_name)
+    print(device["volume_percent"])
+    return device
+
+def vol_up():
+    dev = get_device(device_name)
+    vol = dev["volume_percent"]
+    if vol < 100:
+        vol += 5
+    elif vol >= 100:
+        vol = 100
+    sp.volume(vol, device_id=dev["id"])
+    return
+
+def vol_down():
+    dev = get_device(device_name)
+    vol = dev["volume_percent"]
+    if vol >= 100:
+        vol -= 5
+    elif vol <= 0:
+        vol = 0
+    sp.volume(vol, device_id=dev["id"])
+    return
+
+# def prev_track():
+
+# def play_pause():
+
+# def next_track():
 
 #Play pause key
 VOL_D   = {keyboard.Key.cmd, keyboard.Key.f7}
@@ -14,17 +60,15 @@ def on_press(key):
     if key in VOL_D or VOL_U or PREV or PLAY or NEXT:
         current.add(key)
         if all(k in current for k in VOL_D):
-            print("Volume down")
+            vol_down()
         elif all(k in current for k in VOL_U):
-            print("Volume up")
+            vol_up()
         elif all(k in current for k in PREV):
             print("Prev")
         elif all(k in current for k in PLAY):
             print("Play")
         elif all(k in current for k in NEXT):
             print("Next")
-    if key == keyboard.Key.esc:
-        listener.stop()
 
 def on_release(key):
     try:
