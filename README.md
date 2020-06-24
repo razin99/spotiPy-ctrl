6a93bf1920716b7102b8069bd0f7f8350a7b46fb
Razin Idzuddin <z5281664@unsw.edu.au>
2020-06-24 22:10:26 +1000

    Updated requirements and implemented standard media control

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Control local instance of spotify using shortcuts
 - WinKey + F11 = next
 - WinKey + F07 = vol decrease
 - WinKey + F08 = vol increase
+
+>Pynput combo keys method: https://github.com/moses-palmer/pynput/issues/20 
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# https://github.com/moses-palmer/pynput/issues/20 
 from pynput import keyboard
 import spotipy
 import spotipy.util as util
@@ -12,12 +11,11 @@ token = util.prompt_for_user_token(creds.USERNAME,
                            redirect_uri=creds.REDIRECT_URI)
 
 sp = spotipy.client.Spotify(auth=token)
-# device_name = input("Device name: ")
-device_name = "LAPTOP-T34QML4F"
+device_name = creds.DEVICE_NAME
+
 def get_device(device_name):
     device_list = sp.devices()['devices']
     device = next(dev for dev in device_list if dev["name"] == device_name)
-    print(device["volume_percent"])
     return device
 
 def vol_up():
@@ -28,63 +26,82 @@ def vol_up():
     elif vol >= 100:
         vol = 100
     sp.volume(vol, device_id=dev["id"])
+    print(f"Volume: {vol}")
     return
 
 def vol_down():
     dev = get_device(device_name)
     vol = dev["volume_percent"]
-    if vol >= 100:
+    if vol > 0:
         vol -= 5
     elif vol <= 0:
         vol = 0
     sp.volume(vol, device_id=dev["id"])
+    print(f"Volume: {vol}")
     return
 
-# def prev_track():
+def play_pause():
+    dev = get_device(device_name)
+    try:
+        sp.start_playback(device_id=dev["id"])
+        print("Play")
+    except:
+        sp.pause_playback(device_id=dev["id"])
+        print("Pause")
+    return
 
-# def play_pause():
+def next_track():
+    dev = get_device(device_name)
+    try:
+        sp.next_track(device_id=dev["id"])
+    except:
+        print("Can't go next track")
+    return
 
-# def next_track():
+def prev_track():
+    dev = get_device(device_name)
+    try:
+        sp.previous_track(device_id=dev["id"])
+    except:
+        print("Can't go prev track")
+    return
 
+# Setting up key combinations
+current = set()
 VOL_U = {keyboard.Key.cmd, keyboard.Key.f8}
 VOL_D = {keyboard.Key.cmd, keyboard.Key.f7}
 PREV = {keyboard.Key.cmd, keyboard.Key.f9}
-PLAY = {keyboard.Key.cmd, keyboard.Key.f10}
+PLAY_PAUSE = {keyboard.Key.cmd, keyboard.Key.f10}
 NEXT = {keyboard.Key.cmd, keyboard.Key.f11}
 
-# Active set
-current = set()
-
 def on_press(key):
-    if key in VOL_D or VOL_U or PREV or PLAY or NEXT:
+    if key in VOL_D or VOL_U or PREV or PLAY_PAUSE or NEXT:
         current.add(key)
         if all(k in current for k in VOL_D):
             try:
                 vol_down()
             except:
                 print("Vol down action failed")
-                exit (0)
         elif all(k in current for k in VOL_U):
             try:
                 vol_up()
             except:
                 print("Vol up action failed")
-                exit (0)
-        elif all(k in current for k in PREV):
-            print("Prev")
-        elif all(k in current for k in PLAY):
-            try:
-                sp.start_playback(device_id=get_device(device_name)["id"])
-            except:
-                print("Play pause failed")
+        elif all(k in current for k in PLAY_PAUSE):
+            play_pause()
         elif all(k in current for k in NEXT):
-            print("Next")
+            next_track()
+        elif all(k in current for k in PREV):
+            prev_track()
 
 def on_release(key):
     try:
-        current.remove(key)
+        current.clear()
     except KeyError:
         pass
+
+current.clear()
+print("Listening")
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
